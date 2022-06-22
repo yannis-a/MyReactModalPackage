64e608375336e559207f08f4cb6265dd896fcb4e
Yannis <arcorio.y@gmail.com>
2022-06-22 22:59:50 +0200

    test build & publish npm

---
+++ reactal/dist/components/Modal.js
@@ -0,0 +1,17 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+const Modal = () => {
+  return /*#__PURE__*/_react.default.createElement("div", null);
+};
+
+var _default = Modal;
+exports.default = _default;
