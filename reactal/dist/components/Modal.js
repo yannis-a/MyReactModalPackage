b65626d0813600873d847fb27928747b08b3cccb
Yannis <arcorio.y@gmail.com>
2022-06-27 23:18:57 +0200

    update my package modal

--- reactal/dist/components/Modal.js
+++ reactal/dist/components/Modal.js
@@ -1,16 +1,70 @@
 "use strict";
 
+require("core-js/modules/web.dom-collections.iterator.js");
+
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.default = void 0;
 
-var _react = _interopRequireDefault(require("react"));
+var _reactDom = _interopRequireDefault(require("react-dom"));
+
+var _react = _interopRequireWildcard(require("react"));
+
+var _reactFontawesome = require("@fortawesome/react-fontawesome");
+
+require("./styles/Modal.css");
+
+var _reactTransitionGroup = require("react-transition-group");
+
+var _freeBrandsSvgIcons = require("@fortawesome/free-brands-svg-icons");
+
+function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "function") return null; var cacheBabelInterop = new WeakMap(); var cacheNodeInterop = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(nodeInterop) { return nodeInterop ? cacheNodeInterop : cacheBabelInterop; })(nodeInterop); }
+
+function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-const Modal = () => {
-  return /*#__PURE__*/_react.default.createElement("div", null);
+const Modal = props => {
+  const closeOnEscapeKayDown = e => {
+    if ((e.charCode || e.keyCode) === 27) {
+      props.onClose();
+    }
+  };
+
+  (0, _react.useEffect)(() => {
+    document.body.addEventListener("keydown", closeOnEscapeKayDown);
+    return function cleanup() {
+      document.body.addEventListener("keydown", closeOnEscapeKayDown);
+    };
+  });
+  return /*#__PURE__*/_reactDom.default.createPortal( /*#__PURE__*/_react.default.createElement(_reactTransitionGroup.CSSTransition, {
+    in: props.show,
+    unmountOnExit: true,
+    timeout: {
+      enter: 0,
+      exit: 300
+    }
+  }, /*#__PURE__*/_react.default.createElement("div", {
+    className: "modal",
+    onClick: props.onClose
+  }, /*#__PURE__*/_react.default.createElement("div", {
+    className: "modal-content",
+    onClick: e => e.stopPropagation()
+  }, /*#__PURE__*/_react.default.createElement("div", {
+    className: "modal-header"
+  }, /*#__PURE__*/_react.default.createElement("h4", {
+    className: "modal-title"
+  }, props.title), /*#__PURE__*/_react.default.createElement("div", {
+    className: "modal-close_cross"
+  }, /*#__PURE__*/_react.default.createElement(_reactFontawesome.FontAwesomeIcon, {
+    icon: _freeBrandsSvgIcons.faCircleXmark,
+    onClick: props.onClose
+  }))), /*#__PURE__*/_react.default.createElement("div", {
+    className: "modal-body"
+  }, props.children), /*#__PURE__*/_react.default.createElement("div", {
+    className: "modal-footer"
+  }, props.footer)))), document.getElementById("root"));
 };
 
 var _default = Modal;
--- reactal/src/lib/components/Modal.js
+++ reactal/src/lib/components/Modal.js
@@ -1,11 +1,49 @@
-import React from 'react';
+import ReactDOM from "react-dom";
+import React, { useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./styles/Modal.css";
+import { CSSTransition } from "react-transition-group";
+import { faCircleXmark } from '@fortawesome/free-brands-svg-icons'
 
-const Modal = () => {
-    return (
-        <div>
-            
+
+const Modal = (props) => {
+  const closeOnEscapeKayDown = (e) => {
+    if ((e.charCode || e.keyCode) === 27) {
+      props.onClose();
+    }
+  };
+
+  useEffect(() => {
+    document.body.addEventListener("keydown", closeOnEscapeKayDown);
+    return function cleanup() {
+      document.body.addEventListener("keydown", closeOnEscapeKayDown);
+    };
+  });
+
+  return ReactDOM.createPortal(
+    <CSSTransition
+      in={props.show}
+      unmountOnExit
+      timeout={{ enter: 0, exit: 300 }}
+    >
+      <div className="modal" onClick={props.onClose}>
+        <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+          <div className="modal-header">
+            <h4 className="modal-title">{props.title}</h4>
+            <div className="modal-close_cross">
+              <FontAwesomeIcon
+                icon={faCircleXmark}
+                onClick={props.onClose}
+              />
+            </div>
+          </div>
+          <div className="modal-body">{props.children}</div>
+          <div className="modal-footer">{props.footer}</div>
         </div>
-    );
+      </div>
+    </CSSTransition>,
+    document.getElementById("root")
+  );
 };
 
 export default Modal;
